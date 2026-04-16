9f37ed39e26b1952dde4ac8a95490a001df00add
ShantanuBodkhe <144438033+ShantanuBodkhe@users.noreply.github.com>
2026-04-16 08:12:49 +0530

    Refactor cleanup function in Header component
    Refactor useEffect cleanup to return unsubscribe function directly.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -40,8 +40,8 @@ const Header = () => {
         navigate("/");
       }
     });
-    return unsubscribe; // Cleanup listener on unmount
-  }, []);
+    return () =>unsubscribe();
+  },[]);
 
   return (
     <div className="w-screen absolute px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between items-center">
